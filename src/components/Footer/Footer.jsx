0b4e6b6c00e37bc4cbc81309d108b5c568c36229
LucasA75 <lucasacevedocastro@gmail.com>
2023-07-21 20:51:32 -0400

    feat: add Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="containerFooter">
+      <div className="datosPaginaFooter">
+        <h5>Imaginarios en Escena</h5>
+        <p>klasdnklasnklfdankolsdampsdnklñassa</p>
+      </div>
+      <div className="datosContactoFooter">
+        <h5>Imaginarios en Escena</h5>
+        <p>klasdnklasnklfdankolsdampsdnklñassa</p>
+      </div>
+      <div className="EmailFooter">
+        <h5>Imaginarios en Escena</h5>
+        <p>klasdnklasnklfdankolsdampsdnklñassa</p>
+      </div>
+      <div className="masInfoFooter">
+        <h5>Imaginarios en Escena</h5>
+        <p>klasdnklasnklfdankolsdampsdnklñassa</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

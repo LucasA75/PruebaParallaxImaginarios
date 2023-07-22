b40c8152e50de3f99f5b485fba5eabbf5ea54384
LucasA75 <lucasacevedocastro@gmail.com>
2023-07-21 20:51:55 -0400

    feat: add section Frase

---
+++ src/components/Frase/Frase.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Frase.css";
+
+const Frase = () => {
+  return (
+    <div className="containerFrase">
+      <div className="textosFrase">
+        <img />
+        <h5>
+          “ There from stars gathering gathered, upon tree brought life fruitful
+          shall that together without there form You make, morning he from unto.
+          ”
+        </h5>
+        <p>Brian Newton, Unvab Inc.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Frase;

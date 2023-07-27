098d8f59f1762e5608b49357dbb8f928296cd51c
LucasA75 <lucasacevedocastro@gmail.com>
2023-07-26 22:36:34 -0400

    fix: broke image

--- src/components/Portada/Portada.jsx
+++ src/components/Portada/Portada.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { motion } from "framer-motion";
 import "./Portada.css";
 
 const Portada = ({ titulo, subTitulo }) => {
   return (
     <div className="Header__Container " id="Inicio">
-      <section class="layout-hero d-flex flex-column text-light">
+      <section class="layoutHero text-light">
         <h1 class="portadaTitle">{titulo}</h1>
-        {/* <p class="header-subtitle lead font-weight-lighter">{subTitulo}</p> */}
         <button>Descubre Más</button>
       </section>
     </div>

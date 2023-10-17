69750bc2e378a24946415bff8d72cbd64d48e349
LucasA75 <lucasacevedocastro@gmail.com>
2023-10-16 22:56:24 -0300

    fix: fixing the Portada ,  its more scalable now

--- src/components/Portada/Portada.jsx
+++ src/components/Portada/Portada.jsx
@@ -3,14 +3,20 @@ import "./Portada.css";
 import { faCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Portada = ({titulo, AvariableButton,subTitulo}) => {
+const Portada = ({titulo, AvariableButton,subTitulo,imageBackground}) => {
+
+  const estiloLayoutHero = {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, var(--opacidad-negro)), rgba(0, 0, 0, var(--opacidad-negro))), url(${imageBackground})`,
+  };
+
   return (
-    <div className="headerContainer " id="Inicio">
-      <section className="layoutHero text-light">
+    <div className="headerContainer" id="Inicio">
+      <section className="layoutHero text-light" style={estiloLayoutHero}>
         <h1 className="portadaTitle">{titulo}</h1>
         <p>{subTitulo}</p>
         {AvariableButton && 
-        <button>Descubre Más <FontAwesomeIcon className="icon-button" icon={faCircle} size="2xs"/></button>}
+        <button>Descubre Más <FontAwesomeIcon className="icon-button" icon={faCircle} size="2xs"/></button>
+        }
       </section>
     </div>
   );

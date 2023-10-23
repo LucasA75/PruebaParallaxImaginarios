5944802ca0711327d1792b8553f0fcea89d9466e
LucasA75 <lucasacevedocastro@gmail.com>
2023-10-23 16:52:13 -0300

    Chore: name

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+/* import App from "./App"; */
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import NoPage from "./pages/NoPage/NoPage";
+import ChileEnLaPQ from "./pages/ChileEnLaPQ/ChileEnLaPQ";
+import Nosotros from "./pages/Nosotros/Nosotros";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> ,errorElement:<NoPage/>},
+  { path: "/Chile",element: <ChileEnLaPQ/>},
+  {path:"/Nosotros", element: <Nosotros/>}
+
+]);
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
